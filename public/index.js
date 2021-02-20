f84115f6d07fac1bff8f85f87c1f20085529ee3e
astatsuya <sharekobekko@gmail.com>
2021-02-20 21:31:28 +0900

    add login to client

--- public/index.js
+++ public/index.js
@@ -1,1 +1,24 @@
-console.log("a");
+const handleSubmit = addEventListener("submit", async (e) => {
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  e.preventDefault();
+
+  try {
+    const response = await fetch("http://localhost:3000/users", {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    // statusコードが400, 500系でもエラー扱いされない。
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+    window.alert("error");
+  }
+});
