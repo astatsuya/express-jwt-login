7d7129ee5876401c8dfdb91ceccc4737cb2b4a6b
astatsuya <sharekobekko@gmail.com>
2021-02-20 20:51:01 +0900

    create html and css

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import "./db/index";
 import { logErrors, errorHandler } from "./middleware/errorHandler";
 import * as user from "./routers/user";
@@ -7,6 +8,7 @@ import * as notFound from "./routers/notFound";
 export const app = express();
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(user.router);
 app.use(notFound.router);
 
